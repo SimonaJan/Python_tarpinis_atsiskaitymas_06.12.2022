1c93808e6f0a81992ae6fee523104ca210a56156
Simona <110552634+SimonaJan@users.noreply.github.com>
2022-12-07 18:40:23 +0200

    Add files via upload
    Tarpinis atsiskaitymas

---
+++ Task 1/main.py
@@ -0,0 +1,41 @@
+# Turimas "users" masyvas. 
+
+# Parašykite funkcijas, kurios atlikas nurodytas užduotis:
+# 1. funkcija "filterDogOwers" - kaip argumentą priims masyvą ir duoto masyvo 
+# atveju grąžins "users", kurie turi augintinį.
+# 2. funkcija "filterAdults" - kaip argumentą priims masyvą ir duoto masyvo 
+# atveju grąžins masyvą su "users", kurie yra pilnamečiai.
+
+users = [
+  { "id": '1', "name": 'John Smith', "age": 20, "hasDog": True },
+  { "id": '2', "name": 'Ann Smith', "age": 24, "hasDog": False },
+  { "id": '3', "name": 'Tom Jones', "age": 31, "hasDog": True },
+  { "id": '4', "name": 'Rose Peterson', "age": 17, "hasDog": False },
+  { "id": '5', "name": 'Alex John', "age": 25, "hasDog": True },
+  { "id": '6', "name": 'Ronald Jones', "age": 63, "hasDog": True },
+  { "id": '7', "name": 'Elton Smith', "age": 16, "hasDog": True },
+  { "id": '8', "name": 'Simon Peterson', "age": 30, "hasDog": False },
+  { "id": '9', "name": 'Daniel Cane', "age": 51, "hasDog": True },
+]
+
+def filterDogOwers(dic):
+  return dic["hasDog"] == True
+
+filtered_owners = [owner for owner in users if filterDogOwers(owner)]
+
+print(filtered_owners)
+
+#arba
+# filterDogOwers = [owner for owner in users if owner["hasDog"] == True]
+# print(filterDogOwers)
+
+def filterAdults(dic):
+  return dic["age"] >= 18
+
+filtered_adults = [adult for adult in users if filterAdults(adult)]
+
+print(filtered_adults)
+
+#arba
+# filterAdults = [adult for adult in users if adult["age"] >=18]
+# print(filterAdults)
